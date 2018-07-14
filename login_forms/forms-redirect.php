19612d7896d27f580628b18c6d4bd12e208ce4d5
teja_ps <tejapolisetty143@gmail.com>
2018-07-14 14:47:34 +0530

    login role based

---
+++ login_forms/forms-redirect.php
@@ -0,0 +1,19 @@
+
+
+
+<div class="main-agileits">
+			<div class="new-div-container">
+				<h3> Select Role </h3>
+				
+                <select class="new-divs" id="selectBox"  onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+                        <option value="./" >  Normal User   </option>
+						<option value="./donor.php">  Donor  </option>
+						<option value="./staff.php"> <a href="./staff.php" > staff </a> </option>
+						<option value="./volunteer.php"> <a href="./volunteer.php" > Volunteer </a></option> 
+						<option value="./" >  Normal ser   </option>
+				</select>
+			</div>
+    </div>
+    
+    
+   
