6dd7674595760459dd7a44f53ea567d0af0e60bb
B.Arshia Fathima <arshia188@gmail.com>
2018-07-14 16:11:36 +0530

    login forms updated

---
+++ login_forms/forms-redirect.php
@@ -0,0 +1,24 @@
+
+
+
+<div class="main-agileits">
+			<div class="new-div-container">
+				<h3> Select Role </h3>
+				
+                <select class="new-divs" id="selectBox"  onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+						<option value="./corporate_login.php" >  Corporate   </option>
+						<option value="./student_login.php">  Student  </option>                     
+						<option value="./corporate_login.php" >  Corporate   </option>
+					 <!--
+						<option value="./" >  Normal User   </option>
+						<option value="./donor.php">  Donor  </option>
+						<option value="./staff.php"> <a href="./staff.php" > staff </a> </option>
+						<option value="./volunteer.php"> <a href="./volunteer.php" > Volunteer </a></option> 
+						<option value="./" >  Normal ser   </option>
+						-->
+				</select>
+			</div>
+    </div>
+    
+    
+   
