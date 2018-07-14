c22070b392e8ee53ee66fb6b7755d0c14ae1c904
J Sai Sushma Madhavi <sushirekha2@gmail.com>
2018-07-15 05:06:41 +0530

    Curriculum Pages

---
+++ curriculum_bs3/dashboard_components/sidebar.php
@@ -0,0 +1,74 @@
+<div class="sidebar" data-color="azure" data-image="assets/img/sidebar-5.jpg">
+
+    <!--
+
+        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+        Tip 2: you can also add an image using data-image tag
+
+    -->
+
+    	<div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="#curriculum" class="simple-text">
+                    Curriculum
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li class="active">
+                    <a href="dashboard.php">
+                        <i class="pe-7s-graph"></i>
+                        <p>Sectors</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="sector1.php">
+                        <i class="pe-7s-user"></i>
+                        <p>Banking Sector</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="sector2.php">
+                        <i class="pe-7s-note2"></i>
+                        <p>Sector 2</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="sector3.php">
+                        <i class="pe-7s-note2"></i>
+                        <p>Sector 3</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="pe-7s-news-paper"></i>
+                        <p>Sector 4</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="pe-7s-science"></i>
+                        <p>Sector5</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="pe-7s-map-marker"></i>
+                        <p>sector 6</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="pe-7s-bell"></i>
+                        <p>Notifications</p>
+                    </a>
+                </li>
+				<li class="active-pro">
+                    <a href="upgrade.php">
+                        <i class="pe-7s-rocket"></i>
+                        <p>Upgrade to PRO</p>
+                    </a>
+                </li>
+            </ul>
+    	</div>
+    </div>
