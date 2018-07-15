7bbf149905c1a81b3a1bfde37dc70bed3267efcf
THIRUNAGARI CHANDANA <chandanat19@gmail.com>
2018-07-15 06:58:54 +0530

    Add files via upload

--- exam.php
+++ exam.php
@@ -11,32 +11,47 @@ p {
 </style>
 </head>
 <script>
+
 // Set the date we're counting down to
-var countDownDate = new Date("july 14, 2018 20:30:05").getTime();
+
+var countDownDate = new Date().getTime()+60*5000;
+
 
 // Update the count down every 1 second
+
 var x = setInterval(function() {
 
+
     // Get todays date and time
+
     var now = new Date().getTime();
+
     
     // Find the distance between now an the count down date
+
     var distance = countDownDate - now;
+
     
     // Time calculations for days, hours, minutes and seconds
+
   
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
     
     // Output the result in an element with id="demo"
+
     document.getElementById("demo").innerHTML =  hours + "h "
     + minutes + "m " + seconds + "s ";
+
     
     // If the count down is over, write some text 
+
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+		window.alert("time is finished!");
+      window.location.href="exam2.php";
     }
 }, 1000);
 </script>
@@ -62,75 +77,112 @@ var x = setInterval(function() {
 <form action="verify.php" method="POST">	
    
    <?php
-$level1="level1";$level2="level2";$level3="level3";   
-$con = mysqli_connect("localhost", "root", "", "cfg");
- $count=0;
-if (!$con) {
-    die("ERROR: Could not connect. "
-                .mysqli_connect_error());
-}
- $sqlc = "SELECT count(question) as count from questions ";
-if ($resc = mysqli_query($con, $sqlc)) {
-    if (mysqli_num_rows($resc) > 0) {
-      
-        while ($row = mysqli_fetch_array($resc)) {
-			$count=$row['count'];
-			
-		}
-		
-	}
-	
-}
+   
+$level1 = "level1";
+$level2 = "level2";
+$level3 = "level3";
+session_start();
+$username=$_SESSION["username"];
+// $con = mysqli_connect("localhost", "root", "", "cfg");
 
-$lev="level1";
+include ("../assets/connection.php");
+
+$count = 0;
+
+if (!$conn)
+	{
+	die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+
+$sqlc = "SELECT count(question) as count from questions ";
+
+if ($resc = mysqli_query($conn, $sqlc))
+	{
+	if (mysqli_num_rows($resc) > 0)
+		{
+		while ($row = mysqli_fetch_array($resc))
+			{
+			$count = $row['count'];
+			}
+		}
+	}
+
+$lev = "level1";
 $sql = "SELECT * from questions where level='level1' ORDER BY RAND()";
-if ($res = mysqli_query($con, $sql)) {
-	$i=1;
-    if (mysqli_num_rows($res) > 0) {
-       
-        while ($row = mysqli_fetch_array($res)) 
-		{$id= $row['qid'];
-	     $level=$row['level'];
-	     if($level=="level1")
-		 {
-		
-   ?>
+
+if ($res = mysqli_query($conn, $sql))
+	{
+	$i = 1;
+	if (mysqli_num_rows($res) > 0)
+		{
+		while ($row = mysqli_fetch_array($res))
+			{
+			$id = $row['qid'];
+			$level = $row['level'];
+			if ($level == "level1")
+				{
+?>
 		  	
            
 						
-					<label class="question-label"><?php echo $i ;?></label>	
-				<label ><strong><?php echo $row['question']; ?></label></strong><br>	
-			<input type="radio" name="ans<?php echo $i; ?>" value="a"/>
-							<label ><?php echo $row['optiona']; ?></label><br>
-                         <input type="radio" name="ans<?php echo $i ;?>" value="b"/>
-							<label ><?php echo $row['optionb']; ?></label><br>
-                         <input type="radio" name="ans<?php echo $i; ?>" value="c"/>
-							<label ><?php echo $row['optionc']; ?></label><br>
-                         <input type="radio" name="ans<?php echo $i ;?>" value="d"/>
-							<label ><?php echo $row['optiond']; ?></label><br>	
+			<label class="question-label"><?php
+				echo $i; ?></label>	
+                <label > <textarea style="border: none; width:80%; background-color: transparent;" id="<?php echo $i; ?>"  style="width:50%"><?php echo $row['question']; ?> </textarea>  <div class="button" onclick="speak('<?php echo $i; ?>');">Speak</div> </label>
+                <br/>	
+			<input type="radio" name="ans<?php
+				echo $i; ?>" value="a"/>
+							<label ><?php
+				echo $row['optiona']; ?></label><br />
+                         <input type="radio" name="ans<?php
+				echo $i; ?>" value="b"/>
+							<label ><?php
+				echo $row['optionb']; ?></label><br />
+                         <input type="radio" name="ans<?php
+				echo $i; ?>" value="c"/>
+							<label ><?php
+				echo $row['optionc']; ?></label><br />
+                         <input type="radio" name="ans<?php
+				echo $i; ?>" value="d"/>
+							<label ><?php
+				echo $row['optiond']; ?></label><br />	
 							
 							
 				           
-                           <input type="hidden" name="qid<?php echo $i;?>" value="<?php echo $id;?>" >
+                           <input type="hidden" name="qid<?php
+				echo $i; ?>" value="<?php
+				echo $id; ?>" >
 							
       
         <?php
-		}
-        $i=$i+1;}
-		
-		?>
+				}
+
+			$i = $i + 1;
+			}
+
+?>
 		<center> <input type="submit" name="submit" value="next" style="height:30px"> </center>
 		<?php
-       
-    }
-    else {
-        echo "No matching records are found.";
-    }
-}
-else {
-    echo "ERROR: Could not able to execute $sql. ";
-                                
-}
+		}
+	  else
+		{
+		echo "No matching records are found.";
+		}
+	}
+  else
+	{
+	echo "ERROR: Could not able to execute $sql. ";
+	}
 
-  ?> 
-  
+?> 
+
+<br/><br/><br/><br/><br/><br/>
+<select id="voiceOptions"></select>
+<br/>
+<input type="range" id="volumeSlider" min="0" max="1" value="0.5" step="0.1" />
+<br/>
+<input type="range" id="rateSlider" min="0" max="1" value="0.5" step="0.1" />
+<br/>
+<input type="range" id="pitchSlider" min="0" max="2" value="0.5" step="0.1" />
+<br/>
+            
+<script src="../assets/js/texttoaudio.js"></script>
